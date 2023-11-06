00c107a1df5275098ab9a33da8749e05bcfd8205
Jared Orihuela Contreras <112034917+JaocHatter@users.noreply.github.com>
2023-11-06 04:00:07 -0500

    Add files via upload

---
+++ src/main/java/uni/aed/queue/TestQueueTDA.java
@@ -0,0 +1,40 @@
+package uni.aed.queue;
+
+public class TestQueueTDA {
+    public static void main(String[] args){
+        QueueTDA<String> q=new LinkedQueueTDA<>();
+        String[] temp = {"Jose","Pedro","Sandro"};
+        System.out.println("Despues de insertar elementos en la cola");
+        for(int i=0;i<3;i++){
+            q.add(temp[i]);
+            System.out.println(q.toString());
+        }
+        
+        System.out.println("Despues de eliminar elementos en la cola");
+        for(int i=0;i<3;i++){
+            q.delete();
+            System.out.println(q.toString());
+        }
+    }
+    public static void TestArrayQueueTDA(){
+        QueueTDA<Integer> q=new ArrayQueueTDA<>(5);
+        q.add(30);
+        q.add(10);
+        q.add(20);        
+        System.out.println("Despues de insertar elementos en la cola");
+        System.out.println(q.toString());        
+        q.delete();
+        System.out.println("Despues eliminar");
+        System.out.println(q.toString());
+        q.delete();
+        System.out.println("Despues eliminar");        
+        System.out.println(q.toString());
+        q.add(80);
+        q.add(70);
+        q.add(60);
+        q.add(50);
+        q.add(40);
+        System.out.println("Despues 2 adicion");        
+        System.out.println(q.toString());
+    }
+}
